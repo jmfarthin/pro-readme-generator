a5a6ff814f526a3ae46720f2239d06d64c4f43d7
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-03-23 09:55:59 -0400

    installed inquirer, started work on the generateMarkdown function

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -13,7 +13,41 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
 
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+
+  
+  ## Usage
+  
+
+
+  ## License
+  
+
+  
+  ## Contributing
+  
+
+  
+  ## Tests
+
+
+
+  ## Questions
+
+  
 `;
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # Professional README Generator
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+## Description
+
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+
+  
+  ## Usage
+  
+
+
+  ## License
+  
+
+  
+  ## Contributing
+  
+
+  
+  ## Tests
+
+
+
+  ## Questions
+
